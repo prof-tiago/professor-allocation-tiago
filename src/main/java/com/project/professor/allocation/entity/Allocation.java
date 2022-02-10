9404897c0ad7df4e4163bfbb0ad3539f11bb959f
Tiago santos <professor.tiago.dos.santos@gmail.com>
2022-02-09 21:57:03 -0300

    CLASSE DE CONTROLE DE ALOCAÇÃO

--- src/main/java/com/project/professor/allocation/entity/Allocation.java
+++ src/main/java/com/project/professor/allocation/entity/Allocation.java
@@ -1,5 +1,11 @@
 package com.project.professor.allocation.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.deser.std.DateDeserializers;
+import com.fasterxml.jackson.databind.ser.std.DateSerializer;
+
 import javax.persistence.*;
 import java.time.DayOfWeek;
 import java.util.Date;
@@ -14,10 +20,16 @@ public class Allocation {
     @Column(nullable = false)
     private DayOfWeek day;
 
+    @JsonFormat(pattern = "HH:mmZ")
+    @JsonSerialize(using = DateSerializer.class)
+    @JsonDeserialize(using = DateDeserializers.DateDeserializer.class)
     @Temporal(TemporalType.TIME)
     @Column(nullable = false)
     private Date start;
 
+    @JsonFormat(pattern = "HH:mmZ")
+    @JsonSerialize(using = DateSerializer.class)
+    @JsonDeserialize(using = DateDeserializers.DateDeserializer.class)
     @Temporal(TemporalType.TIME)
     @Column(nullable = false)
     private Date end;
